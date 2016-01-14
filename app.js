b2bd701f2a60315e3456f23193d7cee022f1403a
airburst <mark@fairhursts.net>
2016-01-14 08:47:37 +0000

    Config tests

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ import {default as functions} from 'functionfoundry';
 let expressions = [
     'ABS(-1.1)',
     'LOWER("TEXT ") + ABS(-2)',
-    'SUM(2, 5)',
-    'LEFT("MARK", 2)'
+    'SUM(2, 5)'
 ];
 
 (function() {
